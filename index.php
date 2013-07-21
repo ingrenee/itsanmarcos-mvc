9370655adcfabe1b728fd07f740d56da03f45b6d
ReneeMorales <ReneeMorales@LAPTOP-PC>
2013-07-20 19:18:58 -0500

    Se agregaron las rutinas para la invocacion del controlador y la accion.

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
 <?php
 
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  web.com/?c=usuarios&a=listar
+ *
+ * c: nombre del controlador
+ * a: la accion  del controlador que se va a lanzar
+ *  
+ * 
  */
-?>
+
+$c=$_GET['c']; //es una clase
+$a=$_GET['a']; // es un metodo 
+
+$pathControllers='controllers/'; // ruta donde se encontraran los controladores de mi aplicacion
+$controlador=  ucfirst($c).'Controller';
+$fileController=$pathControllers.$controlador.'.php';
+
+if(file_exists($fileController)):
+        include($fileController);
+    /* si exite el archivo(controlador)*/
+        $obj= new $controlador(); /* instanceando  la clase*/
+        
+        if(method_exists($obj, $a)):
+            /* si existe el metodo en la clase del controlador*/
+            $obj->$a();
+            else:
+            echo ' La accion no existe.';
+        endif;
+
+    
+    else:
+    /* no existe el controlador*/
+    echo 'No existe el controlador';
+endif;
+
+
+
+
+
+
+
+
+        
